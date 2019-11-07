8052eae7de3ce5b05b48e260db97564fb07542ba
Garrett Bischof <gbischof@bnl.gov>
2019-11-07 11:48:44 -0500

    Added some comments

--- remote_test/cat.py
+++ remote_test/cat.py
@@ -12,9 +12,10 @@ class MyDriver(DataSource):
 
     def _get_partition(self, partition):
         print(f'fetching data for {(self._shape, self._color)}')
-        print("PARTITION", partition)
+        print(partition['index'])
         return self._shape, self._color
 
+    # Not sure why this is needed.
     def _get_schema(self):
         return Schema(
             datashape=(2,),
@@ -28,6 +29,7 @@ class InnerCatalog(Catalog):
         self._shape = shape
         super().__init__(**kwargs)
 
+    # Load the entries of the catalog.
     def _load(self):
         print(f'loaded inner catalog for {self._shape}')
         for color in ('red', 'green', 'blue'):
@@ -36,15 +38,19 @@ class InnerCatalog(Catalog):
                 driver='remote_test.cat.MyDriver',
                 description='',
                 catalog=self,
-                direct_access='forbid',
+                direct_access='forbid',  # This needs to be set to forbid.
+                # Args are the kwargs for the MyDriver entries.
                 args={'shape': self._shape, 'color': color})
 
+    # This method loads the data from the source,
+    # and returns the partition defined by the partition argument.
     def read_partition(self, partition):
         print(partition['index'])
 
 
 class OuterCatalog(Catalog):
 
+    # Load the entries of the catalog.
     def _load(self):
         for shape in ('circle', 'square', 'triangle'):
             self._entries[shape] = LocalCatalogEntry(
@@ -52,8 +58,11 @@ class OuterCatalog(Catalog):
                 driver='remote_test.cat.InnerCatalog',
                 description='',
                 catalog=self,
-                direct_access='forbid',
+                direct_access='forbid', # This needs to be set to forbid.
+                # args are the kwargs for the InnerCatalog
                 args={'shape': shape})
 
+    # This method loads the data from the source,
+    # and returns the partition defined by the partition argument.
     def read_partition(self, partition):
         print(partition['index'])
